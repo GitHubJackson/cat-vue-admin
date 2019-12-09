09c7379eedd7ffef0f45ba2df97d117b80bade98
JacksonZhou <1359926897@qq.com>
2019-12-10 01:12:52 +0800

    feat: 打包时去除console语句

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+  configureWebpack: {
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            ecma: undefined,
+            warnings: false,
+            parse: {},
+            compress: {
+              drop_console: true,
+              drop_debugger: false,
+              pure_funcs: ['console.log'] // 移除console
+            }
+          }
+        })
+      ]
+    }
+  }
+}
