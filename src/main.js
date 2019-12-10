8494f550681b103f380eb64c6b1709b019c407de
GithubJackson <1359926897@qq.com>
2019-12-10 10:13:53 +0800

    fix: 引入全局样式

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
 
+import '@/assets/style/index.less'
+
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
